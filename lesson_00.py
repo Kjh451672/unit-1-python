fdf63f1a4307a12648d46395ac71a11912467879
Kyle Heaven <kheaven@brooklynsteamcenter.org>
2024-09-16 10:47:20 -0400

    Benchmark Activity

---
+++ lesson_00.py
@@ -0,0 +1,27 @@
+#1. I created the python file in VSC
+print("Kyle Heaven")
+#2. Next, I printed my name into Python
+fav_show = "The Owl House"
+#3. This variable stores a string wiht the name of my favorite show
+my_float = 3.14
+my_integer = 21
+#4. I created two new variables, one is a integer and one is a float
+print(my_float * my_integer)
+#5. I printed out the product of both previous variables
+favorite_foods = ["Stew Peas","Pizza", "Fried Chicken"]
+#6. I created a list with 3 of my favorite foods in one variable
+drivers_permit = True
+if drivers_permit == True :
+    print("This person can get their liscense.")
+#7. This if statement prints out the statement if the condition is true
+print("What is your name?")
+thier_name = input()
+print("Hello " + thier_name)
+#8. These 3 lines of code ask the user for thier name, and after they input it, the console says hello along with the person's name
+for index in favorite_foods:
+    print (index)
+#9. This for-loop goes through the list i created earlier, and prints out each element one by one
+impossible_loop = -1
+while impossible_loop > 0:
+    print("I am inevitable!")
+#10. I created a while loop that will nevr run, sincethe variable in the conditional statement will never be true
