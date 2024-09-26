edb5c0033d5674b5298048cdf40e7da7d1efb044
Kyle Heaven <kheaven@brooklynsteamcenter.org>
2024-09-26 10:30:36 -0400

    ToDo Tracker

---
+++ todo_tracker.py
@@ -0,0 +1,45 @@
+#Have these variables outside of list so they are unaffected by each new iteration
+to_do_list = []
+while True:
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(to_do_list)
+    print("Here is your todo list. Would you like to add, remove, edit, display or clear all?")
+    option = input()
+
+#Adds a new item to the list
+    if option == "add":
+        print("Please type in your next todo.")
+        next_todo = input()
+        to_do_list.append(next_todo)
+
+#Removes an item from the list
+    if option == "remove":
+        print(to_do_list)
+        print("Please type in the todo # you want to remove")
+        next_remove = int(input())
+        del to_do_list [next_remove - 1]
+
+#Removes all items from the list
+    if option == "clear all":
+        to_do_list.clear()
+
+#Allows user to edit an item in the list
+    if option  == "edit":
+        print(to_do_list)
+        print("Please type in the todo # you want to edit")
+        next_edit = int(input())
+        print("Now please type in the change you want to make.")
+        to_do_list[next_edit - 1] = input() 
+        
+#Displays the entire list
+    if option == "display":
+        num = 1
+        for x in to_do_list:
+            print(f"{num}){x}")
+            num += 1
+
+
+    
+            
+
+    
