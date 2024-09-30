9397b030a32facee833f2cb242024549cc339230
Kyle Heaven <kheaven@brooklynsteamcenter.org>
2024-09-30 09:18:11 -0400

    Finished Todo Tracker

--- todo_tracker.py
+++ todo_tracker.py
@@ -1,9 +1,11 @@
 #Have these variables outside of list so they are unaffected by each new iteration
-to_do_list = []
+with open("my_file.txt") as file:
+    to_do_list = file.readlines()
+
 while True:
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     print(to_do_list)
-    print("Here is your todo list. Would you like to add, remove, edit, display or clear all?")
+    print("Would you like to add, remove, edit, display, clear all or exit out of your list?")
     option = input()
 
 #Adds a new item to the list
@@ -30,16 +32,22 @@ while True:
         next_edit = int(input())
         print("Now please type in the change you want to make.")
         to_do_list[next_edit - 1] = input() 
-        
+
 #Displays the entire list
     if option == "display":
         num = 1
         for x in to_do_list:
             print(f"{num}){x}")
             num += 1
-
-
+#Exits out of todo list while saving contents of list
+    if option == "exit":
+        with open("my_file.txt", "w") as file:
+            file.writelines(to_do_list)
+        break
     
+#Corrects the user if they typed in an incorrect option
+    if option != "exit" or option != "display" or option != "edit" or option != "clear all" or option != "remove" or option != "add":
+        print("Sorry, that doesnt match any of the options. Please try again.")
             
 
     
